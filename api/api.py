1998f9abe6dd97dba477f961811b815aea5ec89e
Quinn Neihaus <Quinn.Neihaus@lise.de>
2023-09-26 14:40:07 +0200

    WIP

--- api/api.py
+++ api/api.py
@@ -1,12 +1,32 @@
-import flask
-from flask import request   # wird benötigt, um die HTTP-Parameter abzufragen
-from flask import jsonify   # übersetzt python-dicts in json
+import sqlite3  
+import flask  
+from flask import request, jsonify
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True  # Zeigt Fehlerinformationen im Browser, statt nur einer generischen Error-Message
+app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Tischreservierung</h1>"
+
+@app.route('/available_seats', methods=['GET'])
+def check_available_seats():
+    conn = sqlite3.connect('../database/tables.db')
+    c = conn.cursor()
+
+    c.execute('''
+        SELECT tische.tischnummer, anzahlPlaetze - IFNULL(sum(case when storniert = "False" then 1 else 0 end), 0) as freiePlaetze
+        FROM tische
+        LEFT JOIN reservierungen
+        ON tische.tischnummer = reservierungen.tischnummer
+        GROUP BY tische.tischnummer
+        HAVING freiePlaetze > 0
+
+    ''')
+    result = c.fetchall()
+
+    conn.close()
+
+    return jsonify(result)
 
 app.run()
